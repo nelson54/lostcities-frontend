dea025f8926f463d234aa7953957b2c13877abb9
Derek Nelson <dereknelson01@gmail.com>
2021-09-01 22:43:16 -0400

    Add bootstrap vue

---
+++ src/components/login/login-service.js
@@ -0,0 +1,35 @@
+export default class LoginService {
+    constructor(url, basepath) {
+
+    }
+
+    login(login, password, rememberMe) {
+        // Default options are marked with *
+        const response = await fetch(_fullPath(), {
+            method: 'POST', 
+            mode: 'cors', 
+            cache: 'no-cache', 
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            referrerPolicy: 'no-referrer',
+            body: JSON.stringify({}) // body data type must match "Content-Type" header
+        });
+        return response.json({login, password, rememberMe}); 
+    }
+
+    _fullPath(path="") {
+        return this.url + this.basepath + fullPath
+    }
+}
+
+let serviceInstance;
+
+export function instance() {
+    if(!serviceInstance) {
+        serviceInstance = LoginService()
+    } 
+
+    return serviceInstance
+}
--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import Vue from "vue";
 import configureRouter from "./router-configurer";
+
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import App from "./App.vue";
 
 Vue.config.productionTip = false;
 
 let router = configureRouter(Vue);
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 new Vue({
   router,
