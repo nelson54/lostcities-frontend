6807a1af5e2b18384b38dd96eea40eb050aae9b8
Derek Nelson <dereknelson01@gmail.com>
2021-09-02 23:00:27 -0400

    Change style of login form

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -1,17 +1,13 @@
 <template>
   <b-form>
-    <b-form-group label="Login" label-for="login" label-size="sm">
-      <b-form-input id="login" v-model="login"></b-form-input>
-    </b-form-group>
-    <b-form-group label="Password" label-for="password" label-size="sm">
-      <b-form-input type="password" id="password"></b-form-input>
-    </b-form-group>
-    <b-form-group label="Remember Me" label-for="remember-me" label-size="sm">
-      <b-form-checkbox v-model="rememberMe" id="remember-me"></b-form-checkbox>
-    </b-form-group>
-    <b-button variant="outline-primary">Login</b-button>
+    
+    <b-form-input id="login" v-model="login" placeholder="Login"></b-form-input>
+    <b-form-input type="password" id="password" placeholder="password"></b-form-input>
+    <b-form-checkbox v-model="rememberMe" id="remember-me"></b-form-checkbox>
+  
+    <b-button variant="outline-primary" class="btn-block">Login</b-button>
     <p>
-      Don't have an account? <router-link to="/register">Register</router-link>
+          Don't have an account? <router-link to="/register">Register</router-link>
     </p>
   </b-form>
 </template>
