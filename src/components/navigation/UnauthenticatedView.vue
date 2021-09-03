69fae919af8e990da306fb263d50c773b51691fc
Derek Nelson <dereknelson01@gmail.com>
2021-09-02 23:45:39 -0400

    styles for buttons

--- src/components/navigation/UnauthenticatedView.vue
+++ src/components/navigation/UnauthenticatedView.vue
@@ -2,10 +2,11 @@
   <div>
     <b-button
       variant="outline-dark"
-      class="position-absolute top-0 start-0 ml-3"
+      class="position-absolute top-0 start-0 ml-3 pl-2 pr-3"
       v-if="showBack()"
       v-on:click="back()"
     >
+      <b-icon-chevron-compact-left />
       Back
     </b-button>
     <b-container id="main" class="bg-light text-dark border my-3 py-3 rounded">
