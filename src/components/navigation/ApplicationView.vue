3ec802e7bb81d32756489d5bd3b16ca5563a2e28
Derek Nelson <dereknelson01@gmail.com>
2021-09-02 23:31:08 -0400

    navbar-nav

--- src/components/navigation/ApplicationView.vue
+++ src/components/navigation/ApplicationView.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <b-navbar type="dark" variant="dark" fixed="top">
       <b-navbar-brand to="/">Home</b-navbar-brand>
-      <b-nav-item to="/login">Login</b-nav-item>
+      <b-navbar-nav>
+        <b-nav-item to="/login">Login</b-nav-item>
+      </b-navbar-nav>
     </b-navbar>
     <div>
       <slot />
