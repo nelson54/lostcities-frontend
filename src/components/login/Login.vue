9caf773f63207e8987f15faf8bbd444d83fea447
Derek Nelson <dereknelson01@gmail.com>
2021-09-02 23:04:58 -0400

    Adding back form groups

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -1,9 +1,14 @@
 <template>
   <b-form>
-    
-    <b-form-input id="login" v-model="login" placeholder="Login"></b-form-input>
-    <b-form-input type="password" id="password" placeholder="password"></b-form-input>
-    <b-form-checkbox v-model="rememberMe" id="remember-me"></b-form-checkbox>
+    <b-form-group>
+      <b-form-input id="login" v-model="login" placeholder="Login"></b-form-input>
+    </b-form-group>
+    <b-form-group>
+      <b-form-input type="password" id="password" placeholder="password"></b-form-input>
+    </b-form-group>
+    <b-form-group>
+      <b-form-checkbox v-model="rememberMe" id="remember-me">Remember Me</b-form-checkbox>
+    </b-form-group>
   
     <b-button variant="outline-primary" class="btn-block">Login</b-button>
     <p>
