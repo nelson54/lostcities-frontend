db4dbe8bcc69792176b39310edea50980fd575bb
Derek Nelson <dereknelson01@gmail.com>
2021-09-02 23:30:12 -0400

    fixed=top

--- src/components/navigation/ApplicationView.vue
+++ src/components/navigation/ApplicationView.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <b-navbar type="dark" variant="dark">
+    <b-navbar type="dark" variant="dark" fixed="top">
       <b-navbar-brand to="/">Home</b-navbar-brand>
       <b-nav-item to="/login">Login</b-nav-item>
     </b-navbar>
