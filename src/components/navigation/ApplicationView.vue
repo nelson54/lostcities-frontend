79b85e59ccfa8886755f6e94488d42e9c5bfe0e9
Derek Nelson <dereknelson01@gmail.com>
2021-09-02 23:29:12 -0400

    Adding styles for main nav.

--- src/components/navigation/ApplicationView.vue
+++ src/components/navigation/ApplicationView.vue
@@ -1,15 +1,9 @@
 <template>
   <div id="app">
-    <b-nav>
-      <ul>
-        <li><router-link to="/">Home</router-link></li>
-        <li><router-link to="/login">Login</router-link></li>
-      </ul>
-    </b-nav>
-    <button id="navigate-btn" @click="navigateAndIncrement">On Success</button>
-    <pre id="counter">{{ n }}</pre>
-    <pre id="query-t">{{ $route.query.t }}</pre>
-    <pre id="hash">{{ $route.hash }}</pre>
+    <b-navbar type="dark" variant="dark">
+      <b-navbar-brand to="/">Home</b-navbar-brand>
+      <b-nav-item to="/login">Login</b-nav-item>
+    </b-navbar>
     <div>
       <slot />
     </div>
